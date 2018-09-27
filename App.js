4a668f0338467640f23e71ce9643837ad530eead
hamzaehsan95 <hamza.ehsan@thedevden.co>
2018-09-27 11:40:57 +0500

    add todo

--- App.js
+++ App.js
@@ -9,9 +9,11 @@ import {
   Platform,
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native';
 import {createStore} from 'redux';
+import {combineReducers} from 'redux';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -33,21 +35,44 @@ const counter  = (state = 0 ,action )=>{
   }
 }
 
+const todo = (state,action)=>{
+switch(action.type){
+  case 'ADD_TODO':
+  return {
+    id:action.id,
+    text:action.text,
+    completed:false
+  };
+  case 'TOGGLE_TODO':
+  if(state.id!== action.id){
+    return state;
+  }
+  return {
+    ...state,
+    completed:!state.completed
+  };
+  default :
+  return state;
+}
+}
 const todos = (state=[],action)=>{
   switch(action.type){
     case 'ADD_TODO':
     return[
       ...state,
-      {
-        id:action.id,
-        text:action.text,
-        completed:false
-      }
+      todo(undefined,action)
     ];
+    case 'TOGGLE_TODO':
+    return state.map(t=>
+      todo(t,action)
+      );
     default:
     return state;
   }
 }
+const todoApp = combineReducers({
+  todos
+});
 
 const testAddTodo = () => {
   const stateBefore = [];
@@ -64,27 +89,44 @@ const testAddTodo = () => {
   }
   ];
 }
-const store = createStore(counter);
+const store = createStore(todoApp);
+
 
 type Props = {};
+let nextid = 0 ;
 export default class App extends Component<Props> {
   render() {
-    store.dispatch({type:'INCREMENT'});
-    testAddTodo();
-    console.log('All tests passed');
-  console.log(store.getState());
+    return (
+     
+      <TodoApp
+      todos = {store.getState().todos}/>
+    );
+  }
+}
+
+class TodoApp extends Component {
+  render() {
     return (
      
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Button onPress={
+          ()=> {
+            store.dispatch({
+              type:'ADD_TODO',
+              text:'Testt',
+              id:nextid++
+            })
+          }
+        } title='ADD todo'>
+        
+        </Button>
+        <View>
+        <Text>
+          {this.props.todos.map(todo=>
+            console.log(todo.id)
+            )};
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        </View>
       </View>
     );
   }
